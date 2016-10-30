3b3bfb39dd6bfad4507ca8e2e7e7acdf495eff6f
Matthew King <mking-clari@users.noreply.github.com>
2016-10-30 08:33:16 -0700

    fix pivot

--- src/tween/mergeDiff.js
+++ src/tween/mergeDiff.js
@@ -1,13 +1,13 @@
 export default function mergeDiff(keys, nextKeys) {
   // Create lookup tables from key to index
-  const extractIndexes = keys => (
+  const extractOrder = keys => (
     keys.reduce((result, k, i) => {
       result[k] = i;
       return result;
     }, {})
   );
-  const indexes = extractIndexes(keys);
-  const nextIndexes = extractIndexes(nextKeys);
+  const order = extractOrder(keys);
+  const nextOrder = extractOrder(nextKeys);
 
   // Create the set of all keys
   const mergedKeys = [].concat(keys, nextKeys).reduce((result, k) => {
@@ -17,25 +17,52 @@ export default function mergeDiff(keys, nextKeys) {
 
   // Sort the set of all keys
   // If k1 and k2 are in the same style array, use their relative order.
-  // Otherwise, put the next style after the current style. (This is an arbitrary convention.)
+  // If k1 and k2 are in different style arrays, order them based on a pivot key that is in both style arrays.
+  // If there isn't a pivot key, put the next style after the current style. (This is an arbitrary convention.)
   const mergedKeyArray = Object.keys(mergedKeys);
   mergedKeyArray.sort((k1, k2) => {
-    if (nextIndexes.hasOwnProperty(k1) && nextIndexes.hasOwnProperty(k2)) {
+    if (nextOrder.hasOwnProperty(k1) && nextOrder.hasOwnProperty(k2)) {
       // k1 and k2 are both in next styles
-      return nextIndexes[k1] - nextIndexes[k2];
+      return nextOrder[k1] - nextOrder[k2];
     }
 
-    if (indexes.hasOwnProperty(k1) && indexes.hasOwnProperty(k2)) {
+    if (order.hasOwnProperty(k1) && order.hasOwnProperty(k2)) {
       // k1 and k2 are both in current styles
-      return indexes[k1] - indexes[k2];
+      return order[k1] - order[k2];
     }
 
-    // k1 and k2 are in different style arrays.
-    if (indexes.hasOwnProperty(k1)) {
+    // k1 and k2 are in different style arrays
+    if (order.hasOwnProperty(k1)) {
       // k1 is in current styles and k2 is in next styles
+      
+      for (let i = 0; i < nextKeys.length; i++) {
+        const k = nextKeys[i];
+        
+        if (order.hasOwnProperty(k)) {
+          if (order[k] > order[k1] && nextOrder[k] < nextOrder[k2]) {
+            return 1;
+          } else if (order[k] < order[k1] && nextOrder[k] > nextOrder[k2]) {
+            return -1;
+          }
+        }
+      }
+      
       return 1;
     } else {
       // k1 is in next styles and k2 is in current styles
+
+      for (let i = 0; i < nextKeys.length; i++) {
+        const k = nextKeys[i];
+
+        if (order.hasOwnProperty(k)) {
+          if (nextOrder[k] > nextOrder[k1] && order[k] < order[k2]) {
+            return 1;
+          } else if (nextOrder[k] < nextOrder[k1] && order[k] > order[k2]) {
+            return -1;
+          }
+        }
+      }
+
       return -1;
     }
   });
