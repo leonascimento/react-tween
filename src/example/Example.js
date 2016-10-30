a56012f6093c028d96cf26e2bab4f89e1273251c
Matthew King <mking-clari@users.noreply.github.com>
2016-10-30 01:19:49 -0700

    simplify example

--- src/example/Example.js
+++ src/example/Example.js
@@ -1,10 +1,8 @@
-import BarChart from './BarChart';
 import classNames from 'classnames';
 import ClickableExample from './ClickableExample';
-import range from 'lodash.range';
 import React from 'react';
 import styles from './Example.scss';
-import TransitionTween from '../tween/TransitionTween';
+import Tween from '../tween/Tween';
 
 export default function Example({ className, ...props }) {
   return (
@@ -14,58 +12,17 @@ export default function Example({ className, ...props }) {
     >
       <ClickableExample>
         {(flag, onFlag) => (
-          <TransitionTween
-            sortKey={d => d}
-            styles={range(5)
-              .map(k => ({
-                key: k.toString(),
-                style: {
-                  position: k,
-                  value: (flag ? [50, 70, 90, 110, 130] : [50, 100, 150, 100, 50])[k],
-                },
-                data: k,
-              }))
-            }
-            willEnter={() => ({ value: 0 })}
-            willLeave={() => ({ value: 0 })}
-          >
-            {barStyles => (
-              <BarChart
+          <Tween style={{ opacity: (flag ? 0.5 : 1) }}>
+            {style => (
+              <div
                 className={styles.example}
                 onClick={onFlag}
-                styles={barStyles}
-              />
+                style={style}
+              >
+                Click to animate opacity
+              </div>
             )}
-          </TransitionTween>
-        )}
-      </ClickableExample>
-      <ClickableExample>
-        {(flag, onFlag) => (
-          <TransitionTween
-            sortKey={d => d.index}
-            styles={[50, 70, 90, 110, 130]
-              .map((value, i) => ({ index: i, value }))
-              .filter(d => flag ? (d.index % 2 === 1) : true)
-              .map((d, i) => ({
-                key: d.index.toString(),
-                style: {
-                  position: i,
-                  value: d.value,
-                },
-                data: d,
-              }))
-            }
-            willEnter={() => ({ value: 0 })}
-            willLeave={() => ({ value: 0 })}
-          >
-            {barStyles => (
-              <BarChart
-                className={styles.example}
-                onClick={onFlag}
-                styles={barStyles}
-              />
-            )}
-          </TransitionTween>
+          </Tween>
         )}
       </ClickableExample>
     </div>
